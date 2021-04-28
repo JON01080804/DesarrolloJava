905e37cd807c7ef8317fa4c4aae0f84805f99f42
cursos_externos <cursos_externos@HZ327424.mshome.net>
2021-04-28 12:00:06 +0200

    280421

--- src/com/elorrieta/videojuego/EjecutarJuego.java
+++ src/com/elorrieta/videojuego/EjecutarJuego.java
@@ -7,10 +7,10 @@ public class EjecutarJuego {
 		Guerrero g1 = new Guerrero("Adam", 100, "Espada");
 		System.out.println(g1);
 		g1.cantar();
-		System.out.println("Mi personaje se llama: "+ g1.getNombre()+ /"Su energia es:"+g1.getEnergia()+"y su arma es"+g1.getArma());
 
 		Mago m1 = new Mago("Merlin", 100, "Invisible");
 //		System.out.println(m1);
+		
 
 	}
 
